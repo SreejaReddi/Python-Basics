cf5d2eb91ba7a9b092ce149b53d90c00e009f495
Sreeja Reddy <103733079+SreejaReddi@users.noreply.github.com>
2022-04-15 17:39:53 +0530

    Add files via upload

---
+++ ordered.py
@@ -0,0 +1,14 @@
+from collections import OrderedDict
+d=OrderedDict()
+d[1]='s'
+d[2]='r'
+d[3]='e'
+d[4]='e'
+d[5]='j'
+d[6]='a'
+print(d)
+#ordered dictionary keys
+print(d.keys())
+#replacing the value
+d[1]='p'
+print(d)

8835da29cf00a4b171927c8411075af1fb1c465d
Sreeja Reddy <103733079+SreejaReddi@users.noreply.github.com>
2022-04-15 17:39:39 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,65 @@
+a=10
+b=30
+#arithmetic
+print(a+b)
+print(a*b)
+print(a/b)
+print(a-b)
+print(a%b)
+print(a**b)
+#assignment
+c=5
+c+=5
+c*=5
+c-=5
+print(c)
+#comparision
+compare=a==b
+compare=a!=b
+compare=a<=b
+compare=a>=b
+compare=a>b
+compare=a<b
+print(compare)
+#conditional
+s=10
+r=10
+if a==b:
+    print(a)
+#logical
+x=20
+print(x>20 and x<5)
+print(x>20 or x<5)
+print(not(x>20 and x<5))
+#identity
+list1=[10,20,30]
+list2=[10,10,10]
+y=list1
+print(y is list1)
+print(y is not list2)
+print(y in list1)
+print(10 in list1)
+print(list1==list2)
+#bitwise
+print(10&12)
+print(10>>12)
+print(10<<12)
+print(10|12)
+print(10^12)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
